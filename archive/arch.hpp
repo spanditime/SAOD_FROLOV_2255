bdb469c695a1488f316070c859e9a2416b72acbb
Eugene Frolov <spanditime@gmail.com>
2023-03-10 16:25:39 +0300

    Add auxiliary class BitReader and refactor arch.hpp

--- archive/arch.hpp
+++ archive/arch.hpp
@@ -2,19 +2,44 @@
 #include <iostream>
 #include <map>
 
-uint8_t getBit(uint8_t*data,uint64_t bytesoffset,uint8_t bitsoffset){
-	return (data[bytesoffset] >> (7-bitsoffset)) & 0x01;
-}
-
-uint8_t getBits(uint8_t*data,uint64_t bytesoffset,uint8_t bitsoffset,uint8_t len){
-	uint8_t ret = 0x00;
-	for(uint8_t i= 0; i < len; i++){
-		ret |= getBit(data,bytesoffset + (bitsoffset+i)/8,(bitsoffset+i)%8);
-		ret <<=1;
+class BitReader{
+private:
+	uint8_t*data;
+	std::size_t size;
+	uint64_t bytesoffset=0;
+	uint64_t bitsoffset=0;
+public:
+	BitReader(uint8_t*Data,std::size_t Size){
+		data = Data;
+		size = Size;
 	}
-	ret >>= 1;
-	return ret;
-}
+	uint8_t getBit(){
+		uint8_t ret = (data[bytesoffset] >> (7-bitsoffset)) & 0x01;
+		bitsoffset += 1;
+		bytesoffset += bitsoffset / 8;
+		bitsoffset %= 8;
+		return ret;
+	}
+	uint8_t getBits(uint8_t len){
+		uint8_t ret = 0x00;
+		for(uint8_t i= 0; i < len; i++){
+			ret |= getBit();
+			ret <<=1;
+		}
+		ret >>= 1;
+		return ret;
+	}
+	void moveBytes(uint64_t bytes){
+		bytesoffset += bytes;
+	}
+	void moveBits(uint8_t bits){
+		bytesoffset += bits / 8;
+		bitsoffset += bits % 8;
+		bytesoffset += bitsoffset / 8;
+		bitsoffset %= 8;
+	}
+};
+
 
 class Archive{
 public:
@@ -26,28 +51,22 @@ public:
 		uint8_t n = 0;
 		for(uint8_t t = kwmsize;t;t >>=1,n++);
 		uint8_t alps = data[9];
+		BitReader reader = BitReader(data,size);
+		reader.moveBytes(10);
 
 		std::cout<<"max size of kword = "<<uint64_t(kwmsize)<<std::endl << "alphabet len = " << uint64_t(alps) << std::endl;
 		std::cout<<"n = " << uint64_t(n) << std::endl;
 
-		uint64_t bytesoffset = 10,bitsoffset = 0;
 
 		char*message = new char[letters+1];
 
 		// decompress an alphabet
 		std::map<std::pair<uint8_t,uint8_t>,char> alphabet; // [<Kword,size of kword in bits>] = ascii character
 		for(uint8_t i = 0; i < alps; i++){
-			char c = getBits(data,bytesoffset,bitsoffset,8);
-			bytesoffset += 1;
+			char c = reader.getBits(8);
 			uint8_t m, kword;
-			m = getBits(data, bytesoffset,bitsoffset,n);
-			bitsoffset += n;
-			bytesoffset += bitsoffset/8;
-			bitsoffset %=8;
-			kword = getBits(data, bytesoffset,bitsoffset,m);
-			bitsoffset += m;
-			bytesoffset += bitsoffset/8;
-			bitsoffset %=8;
+			m = reader.getBits(n);
+			kword = reader.getBits(m);
 			alphabet[std::make_pair(kword,m)] = c;
 			std::cout<< c << "- size="<<uint64_t(m)<<" kword="<<uint64_t(kword) << std::endl;
 		}
@@ -57,10 +76,7 @@ public:
 			uint8_t kword=0,m=0;
 			do{
 				kword <<=1;
-				kword |= getBit(data,bytesoffset, bitsoffset);
-				bitsoffset += 1;
-				bytesoffset += bitsoffset/8;
-				bitsoffset %=8;
+				kword |= reader.getBit();
 				m+=1;
 				if(alphabet.count(std::make_pair(kword, m))){
 					break;
