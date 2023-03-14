782bc2d3adac9868e09d5869539ddfb46245de3d
Eugene Frolov <spanditime@gmail.com>
2023-03-14 11:16:02 +0300

    Fix consistency between reading and writing, finish compressing alg, remove debug print

--- archive/arch.hpp
+++ archive/arch.hpp
@@ -3,6 +3,9 @@
 #include <map>
 #include <cstring>
 #include <fstream>
+#include <vector>
+#include <algorithm>
+#include <bitset>
 
 class BitReader{
 private:
@@ -16,7 +19,7 @@ public:
 		size = Size;
 	}
 	uint64_t getBit(){
-		uint64_t ret = (data[bytesoffset] >> (bitsoffset)) & 0x01;
+		uint64_t ret = (data[bytesoffset] >> (7-bitsoffset)) & 0x01;
 		bitsoffset += 1;
 		bytesoffset += bitsoffset / 8;
 		bitsoffset %= 8;
@@ -25,9 +28,16 @@ public:
 	uint64_t getBits(uint8_t len){
 		uint64_t ret = 0x00;
 		for(uint8_t i= 0; i < len; i++){
-			ret |= (getBit() << i);
+			ret <<=1;
+			ret |= getBit();
 		}
 		return ret;
+	}
+	uint64_t getBitOffset(){
+		return bitsoffset;
+	}
+	uint64_t getByteOffset(){
+		return bytesoffset;
 	}
 	void moveBytes(uint64_t bytes){
 		bytesoffset += bytes;
@@ -55,27 +65,82 @@ public:
 		fst = std::ofstream(filename, std::ios::binary);
 	}
 	void writeBit(uint8_t bit){
-		byte >>=1;
+		byte |= (bit & 0b1) << (7-fill); // fixed as it should be 
 		fill += 1;
-		byte |= (bit & 1) << 7;
 		if(fill == 8){
 			writeByte();
 		}
 	}
 	void writeBits(uint64_t bits, uint8_t len){
 		for(uint8_t i = 0; i < len; i++){
-			writeBit(bits);
-			bits >>=1;
+			writeBit(bits >> (len - 1 - i));
 		}
 	}
 	void close(){
 		if(fill){
-			byte >>= 8-fill;
 			writeByte();
 		}
 		fst.close();
 	}
 };
+
+class HuffmansTree{
+private:
+	HuffmansTree *lc, *rc;
+	uint64_t weight=0;
+	char ch;
+	bool is_ch;
+	void constructRecursive(std::map<char,std::pair<uint64_t,uint8_t>> &mp,uint64_t code,uint8_t length) const {
+		if(is_ch){
+			mp[ch] = std::make_pair(code,length);
+		}else{
+			if(lc != nullptr){
+				lc->constructRecursive(mp, code << 1, length + 1);
+			}
+			if(rc != nullptr){
+				rc->constructRecursive(mp, (code << 1) + 1, length + 1);
+			}
+		}
+	}
+public:
+	HuffmansTree(char c,uint64_t w){
+		ch = c; weight = w; is_ch = true;
+	}
+	HuffmansTree(HuffmansTree *l, HuffmansTree *r){
+		lc = l; rc = r; is_ch = false;
+		if(lc != nullptr){
+			weight += lc->weight;
+		}
+		if(rc != nullptr){
+			weight += rc->weight;
+		}
+	}
+	~HuffmansTree(){
+		if(lc != nullptr){
+			delete lc;
+		}
+		if(rc != nullptr){
+			delete rc;
+		}
+	}
+	std::map<char,std::pair<uint64_t,uint8_t>> constructKwordMap() const {
+		std::map<char,std::pair<uint64_t,uint8_t>> ret;
+		constructRecursive(ret, 0, 0);
+		return ret;
+	}
+	const bool is_char() const{
+		return is_ch;
+	}
+	const uint64_t getWeight() const{
+		return weight;
+	}
+};
+
+inline uint8_t calculate_n(const uint8_t &kwmsize){
+	uint8_t n=0;
+	for(uint8_t t = kwmsize;t;t >>=1,n++);
+	return n;
+}
 
 class Archive{
 public:
@@ -95,58 +160,58 @@ public:
 		return decompressRaw(buffer,length);
 	}
 	static char* decompressRaw(uint8_t* data, std::size_t size) {
-		uint64_t letters = ((uint64_t*)data)[0];
-		std::cout<< "letter = "<<letters<<std::endl;
-
-		uint8_t kwmsize = data[8];
-		uint8_t n = 0;
-		for(uint8_t t = kwmsize;t;t >>=1,n++);
-		uint8_t alps = data[9];
 		BitReader reader = BitReader(data,size);
-		reader.moveBytes(10);
+		uint64_t letters = 0;
+		for(uint8_t i = 0; i < sizeof(uint64_t); i++){
+			uint8_t fr = reader.getBits(8);
+			((uint8_t *)&letters)[sizeof(uint64_t) - 1 - i] = fr;
+		}
 
-		std::cout<<"max size of kword = "<<uint64_t(kwmsize)<<std::endl << "alphabet len = " << uint64_t(alps) << std::endl;
-		std::cout<<"n = " << uint64_t(n) << std::endl;
-
+		uint8_t kwmsize = reader.getBits(8);
+		uint8_t n = calculate_n(kwmsize);
+		uint8_t alps = reader.getBits(8);
 
 		char*message = new char[letters+1];
 
 		// decompress an alphabet
-		std::map<std::pair<uint8_t,uint8_t>,char> alphabet; // [<Kword,size of kword in bits>] = ascii character
+		std::map<std::pair<uint64_t,uint8_t>,char> alphabet; // [<Kword,size of kword in bits>] = ascii character
 		for(uint8_t i = 0; i < alps; i++){
 			char c = reader.getBits(8);
 			uint8_t m, kword;
 			m = reader.getBits(n);
 			kword = reader.getBits(m);
 			alphabet[std::make_pair(kword,m)] = c;
-			std::cout<< c << "- size="<<uint64_t(m)<<" kword="<<uint64_t(kword) << std::endl;
 		}
 
-		std::cout<<"Message:\n";
+		// decompress a message
 		for(uint64_t l = 0; l < letters; l++){
-			uint8_t kword=0,m=0;
+			uint64_t kword=0;
+			uint8_t m=0;
 			do{
-				kword |= reader.getBit() << m;
+				kword <<=1;
+				kword |= reader.getBit();
 				m+=1;
 				if(alphabet.count(std::make_pair(kword, m))){
 					break;
 				}
 				if(m>kwmsize){
-					std::cout<<"EERRRROORR\n";
-					return NULL;
+					message[l] = '\0';
+					// throw an error
+					return message;
 				}
 			}while(true);
 			message[l] = alphabet[std::make_pair(kword, m)];
 		}
-		message[letters] = '\0';
 
-		std::cout<< message << std::endl;
 		return message;
 	}
 	
-	void compress(const char*message,const char*filename){
-		std::map<char, uint> weighted;
-		std::size_t stlen = std::strlen(message);
+	static void compress(const char*message,const char*filename){
+		//open BitStream for writing
+		BitWriter bwa(filename);
+
+		std::map<char, uint64_t> weighted;
+		uint64_t stlen = std::strlen(message);
 		for(uint i = 0; i < stlen; i++){
 			if(weighted.count(message[i])){
 				weighted[message[i]] += 1;
@@ -155,6 +220,69 @@ public:
 			}
 		}
 
+		std::vector<HuffmansTree *> Huffman;
+		for(auto n : weighted){
+			Huffman.push_back(new HuffmansTree(n.first, n.second));
+		}
 
+		while(Huffman.size() > 1){
+			HuffmansTree * min1, *min2;
+			if(Huffman[0]->getWeight() < Huffman[1]->getWeight()){
+				min1 = Huffman[0]; min2 = Huffman[1];
+			}else{
+				min1 = Huffman[1]; min2 = Huffman[0];
+			}
+			for(uint64_t i = 2; i < Huffman.size();i++){
+				if(Huffman[i]->getWeight() < min2->getWeight()){
+					if(Huffman[i]->getWeight() < min1->getWeight()){
+						min2 = min1;
+						min1 = Huffman[i];
+					}else{
+						min2 = Huffman[i];
+					}
+				}
+			}
+			Huffman.erase(std::remove(Huffman.begin(), Huffman.end(),min1), Huffman.end());
+			Huffman.erase(std::remove(Huffman.begin(), Huffman.end(),min2), Huffman.end());
+			Huffman.push_back(new HuffmansTree(min1,min2));
+		}
+		if(Huffman[0]->is_char()){
+			Huffman[0] = new HuffmansTree(Huffman[0],nullptr);
+		}
+
+		auto kwmp = Huffman[0]->constructKwordMap();
+		uint8_t maxkwsize = (*kwmp.begin()).second.second;
+		uint8_t minkwsize = maxkwsize;
+		for(auto n : kwmp){
+			if(n.second.second > maxkwsize){
+				maxkwsize = n.second.second;
+			} else if(n.second.second < minkwsize){
+				minkwsize = n.second.second;
+			}
+		}
+		uint8_t n = calculate_n(maxkwsize);
+		
+		// Start writing header
+		// write length
+		bwa.writeBits(stlen, 64);
+		// mkwsize
+		bwa.writeBits(maxkwsize,8);
+		// alphabet size
+		bwa.writeBits(kwmp.size(),8);
+		//Alphabet
+		for(auto lt = kwmp.begin(); lt != kwmp.end(); ++lt){
+			bwa.writeBits((*lt).first, 8);// ascii
+			bwa.writeBits((*lt).second.second, n); // kword length in bits
+			bwa.writeBits((*lt).second.first, (*lt).second.second); // kword
+		}
+		// write message
+		
+		for(uint64_t i = 0; i < stlen; i++){
+			char c = message[i];
+			uint64_t kw = kwmp[c].first;
+			uint8_t sz = kwmp[c].second;
+			bwa.writeBits(kw , sz);
+		}
+		bwa.close();
 	}
 };
--- archive/main.cpp
+++ archive/main.cpp
@@ -3,37 +3,8 @@
 
 
 int main(){
-	BitWriter bwa("a.compressed");
-	bwa.writeBits(2, 64); // size in letters = 2
-	bwa.writeBits(2,8); // max size of keyword
-	bwa.writeBits(2,8); // size of alphabet
-	bwa.writeBits('H',8);
-	bwa.writeBits(0b10,2); // size of kword
-	bwa.writeBits(0b00,0b10); // kword
-	bwa.writeBits('i', 8); 
-	bwa.writeBits(0b10, 2); // size of kword
-	bwa.writeBits(0b10, 0b10); // kword
-	bwa.writeBits(0b00,2); // kword for H
-	bwa.writeBits(0b10,2); // kword for i
-	bwa.close();
-
-	// total size of a message is 4 bits not 16, but there is also 4 of empty bits that have to be there
-	// 24 bits of huffman tree and 80 bits of utility data 112 bits in total
-	uint8_t s[]{
-		0x0A,0x00,0x00,0x00,0x00,0x00,0x00,0x00, // size in letters = 10
-		0x02, //max size of keyword
-		0x03, // size of alphabet
-		//first letter of aplhabet
-		0x48, // for H 
-		0b10010010,// 10=2 - size of kword, 00 - kword
-		0b10100110,// 01101001 - ascii "i", 10=2 - size of kword, 10-keyword
-		0x21, // for !
-		0b10111110, // 10=2 - size of kword, 11 - kword/ message starts 11 - ! 10 - i
-		0b11100000, // HHi!
-		0b11111000, // Hi!!
-	};
-	// total size of message is 20 < 80, but in total with all utility data needed to decompress its 136 bits
-
-	Archive::decompress("a.compressed");
-	Archive::decompressRaw(s, 17);
+	// Archive::decompress("a.compressed");
+	// Archive::decompressRaw(s, 17);
+	Archive::compress("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer sed ante in elit blandit ultricies. Vivamus efficitur sapien non lacus accumsan, vitae iaculis sem porttitor. Curabitur eu neque at quam sollicitudin faucibus quis convallis eros. In pharetra ligula ut tortor mollis tempor. Phasellus lectus orci, egestas in nulla at, feugiat posuere leo. Mauris sagittis, odio vitae pellentesque tristique, enim massa gravida arcu, quis tincidunt augue mauris vel tortor. Nullam bibendum lectus vitae erat egestas finibus. Nunc in auctor sapien. Vestibulum ultricies, tellus nec tempor luctus, purus lectus ornare metus, sed dignissim augue velit consectetur metus.","b.compressed");
+	std::cout<< "decompressed message:\n" << Archive::decompress("b.compressed")<<std::endl;
 }
